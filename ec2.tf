990a2a7d778249c09804b6623c8dfeb8830131d4
Pavanarama <55379489+Pavanarama@users.noreply.github.com>
2022-06-01 20:34:58 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAXI6ADCU3PJ4VCZ5P"
-  secret_key = "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"
+
 }
 
 resource "aws_instance" "Ec2" {
